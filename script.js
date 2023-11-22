b0ea47751b6a431540f2ad62954801517d375228
ScottGrieveWeb <scottgrieveweb@gmail.com>
2023-11-22 18:09:18 +0000

    Began creating logic for the playRound function
    -Utilising a nested switch statement within an if statement, I
    have created the necessary logic cases for a player selection of
    "Rock".
    -This logic has been tested and is working with no issues.

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
 
     if (randomNumber === 1){
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 2){
-        return "Scissors";
+        return "scissors";
     } else {
-        return "Paper";
+        return "paper";
     }
 }
 
@@ -20,4 +20,25 @@ function playerSelection(){
     return playerChoice;
 }
 
+function playRound(playerAnswer, computerSelection) {
+    if (playerAnswer == "rock") {
+        switch(computerSelection){
+            case "rock":
+                alert("You draw! Rock ties with Rock.");
+                break;
+            case "paper":
+                alert("You lose! Paper beats Rock.");
+                break;
+            case "scissors":
+                alert("You win! Rock beats Scissots.");
+                break;
+        }
+    }
+}
+
+const playerAnswer = playerSelection();
+const computerSelection = getComputerChoice();
+console.log(playerAnswer);
+console.log(computerSelection);
+console.log(playRound(playerAnswer, computerSelection));
 
